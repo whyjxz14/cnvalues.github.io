76f8e1b2034b077b3445bb63fe611171ce78f10e
CNValues <6501773-CNValues@users.noreply.gitlab.com>
2020-08-04 10:43:09 +0800

    0.3.7 b
    Add new statements and ideologies.

--- ideologies.js
+++ ideologies.js
@@ -6,8 +6,8 @@ ideologies = [
             "govt": 100,
             "scty": 90
         },
-        "desc": NaN,
-        "link": NaN
+        "desc": "无政府共产主义是无政府主义中最主要的流派，主张废除国家机器和私有制，在自愿和互助的基础上建立一个各尽所能，按需分配的公有制社会。",
+        "link": "https://en.wikipedia.org/wiki/Anarcho-communism"
     },
     {
         "name": "托洛茨基主义",
@@ -16,98 +16,58 @@ ideologies = [
             "govt": 50,
             "scty": 90
         },
-        "desc": NaN,
-        "link": NaN
+        "desc": "托洛茨基主义坚持布尔什维克革命初期宣告的工人民主和国际主义原则，强烈反对官僚化和一国社会主义论。",
+        "link": "https://en.wikipedia.org/wiki/Trotskyism"
+    },
+    {
+        "name": "正统马克思主义",
+        "stats": {
+            "econ": 100,
+            "govt": 70,
+            "scty": 90
+        },
+        "desc": "正统马克思主义坚持社会主义民主和政治自由，同时强调激进的社会主义革命的必要性，反对官僚化的集中制和改良主义。",
+        "link": "https://en.wikipedia.org/wiki/Orthodox_Marxism"
     },
     {
         "name": "列宁主义",
         "stats": {
             "econ": 100,
             "govt": 30,
-            "scty": 70
+            "scty": 60
         },
-        "desc": NaN,
-        "link": NaN
+        "desc": "列宁主义主张建立民主集中制的先锋党并通过革命推翻资本主义，为了实现目标，不排斥临时的强制政策。",
+        "link": "https://en.wikipedia.org/wiki/Leninism"
     },
     {
-        "name": "斯大林主义",
+        "name": "马克思列宁主义",
         "stats": {
-            "econ": 100,
-            "govt": 0,
-            "scty": 40
+            "econ": 95,
+            "govt": 5,
+            "scty": 30
         },
-        "desc": NaN,
-        "link": NaN
-    },
-    {
-        "name": "卢森堡主义",
-        "stats": {
-            "econ": 100,
-            "govt": 70,
-            "scty": 90
-        },
-        "desc": NaN,
-        "link": NaN
+        "desc": "马克思列宁主义是斯大林时期苏联发展出的理论体系，其特点是一党专政，严厉的思想文化管制，一国社会主义论以及高度集中的计划经济。",
+        "link": "https://en.wikipedia.org/wiki/Marxism%E2%80%93Leninism"
     },
     {
         "name": "自由意志社会主义",
         "stats": {
             "econ": 85,
             "govt": 90,
-            "scty": 80
+            "scty": 85
         },
-        "desc": NaN,
-        "link": NaN
+        "desc": "自由意志社会主义是社会主义运动中强调反对由中央集权国家控制社会和经济，主张基层工人自治的流派。",
+        "link": "https://en.wikipedia.org/wiki/Libertarian_socialism"
     },
     {
         "name": "民主社会主义",
         "stats": {
-            "econ": 85,
-            "govt": 75,
+            "econ": 80,
+            "govt": 70,
             "scty": 80
         },
-        "desc": NaN,
-        "link": NaN
-    },
-    {
-        "name": "铁托主义",
-        "stats": {
-            "econ": 80,
-            "govt": 20,
-            "scty": 75
-        },
-        "desc": NaN,
-        "link": NaN
-    },
-    {
-        "name": "社会民主主义",
-        "stats": {
-            "econ": 75,
-            "govt": 65,
-            "scty": 70
-        },
-        "desc": NaN,
-        "link": NaN
-    },
-    {
-        "name": "民族布尔什维克主义",
-        "stats": {
-            "econ": 75,
-            "govt": 5,
-            "scty": 5
-        },
-        "desc": NaN,
-        "link": NaN
-    },
-    {
-        "name": "左翼民粹主义",
-        "stats": {
-            "econ": 70,
-            "govt": 30,
-            "scty": 50
-        },
-        "desc": NaN,
-        "link": NaN
+        "desc": "民主社会主义强调民主制在社会主义社会中的必要性，既反对先锋党模式，也不认同过激的无政府主义倾向。",
+        "link": "https://en.wikipedia.org/wiki/Democratic_socialism"
     },
     {
         "name": "左翼自由意志主义",
@@ -116,68 +76,108 @@ ideologies = [
             "govt": 80,
             "scty": 80
         },
-        "desc": NaN,
-        "link": NaN
+        "desc": "左翼自由意志主义在坚持自由意志主义的同时抵制资本主义的私有制和雇佣劳动，尤其反对私人占有自然资源。",
+        "link": "https://en.wikipedia.org/wiki/Left-libertarianism"
     },
     {
-        "name": "社会自由主义",
+        "name": "社会民主主义",
         "stats": {
-            "econ": 60,
+            "econ": 70,
             "govt": 60,
             "scty": 70
         },
-        "desc": NaN,
-        "link": NaN
+        "desc": "社会民主主义主张对资本主义制度进行渐进式的改良，并强调这一改良运动本身的意义，而非特定的目标。",
+        "link": "https://en.wikipedia.org/wiki/Social_democracy"
+    },
+    {
+        "name": "左翼民粹主义",
+        "stats": {
+            "econ": 70,
+            "govt": 25,
+            "scty": 50
+        },
+        "desc": "左翼民粹主义主张加强国家对社会和经济的管控以保护底层民众，并且反对政治精英阶层的议会政治。",
+        "link": "https://en.wikipedia.org/wiki/Left-wing_populism"
+    },
+    {
+        "name": "民族布尔什维克主义",
+        "stats": {
+            "econ": 70,
+            "govt": 0,
+            "scty": 5
+        },
+        "desc": "民族布尔什维克主义主张将苏联式的政治经济体制与法西斯的民族主义理念结合起来，反对民主，私有经济和国际主义。",
+        "link": "https://en.wikipedia.org/wiki/National_Bolshevism"
     },
     {
         "name": "互助主义",
         "stats": {
             "econ": 60,
             "govt": 100,
+            "scty": 60
+        },
+        "desc": "无政府互助主义主张在自由市场的框架下将生产资料的使用权社会化，同时反对资本主义雇佣劳动。",
+        "link": "https://en.wikipedia.org/wiki/Mutualism_(economic_theory)"
+    },
+    {
+        "name": "社会自由主义",
+        "stats": {
+            "econ": 55,
+            "govt": 60,
             "scty": 70
         },
+        "desc": "社会自由主义是一种更加倾向于社会公平及经济干预的自由主义，并且尤其重视社会和文化的进步。",
+        "link": "https://en.wikipedia.org/wiki/Social_liberalism"
+    },
+    {
+        "name": "纳粹主义",
+        "stats": {
+            "econ": 50,
+            "govt": 0,
+            "scty": 0
+        },
         "desc": NaN,
-        "link": NaN
+        "link": "https://en.wikipedia.org/wiki/Nazism"
+    },
+    {
+        "name": "基督教民主主义",
+        "stats": {
+            "econ": 50,
+            "govt": 45,
+            "scty": 40
+        },
+        "desc": NaN,
+        "link": "https://en.wikipedia.org/wiki/Christian_democracy"
     },
     {
         "name": "自由主义",
         "stats": {
             "econ": 45,
-            "govt": 60,
+            "govt": 55,
             "scty": 60
         },
         "desc": NaN,
-        "link": NaN
-    },
-    {
-        "name": "纳粹主义",
-        "stats": {
-            "econ": 45,
-            "govt": 0,
-            "scty": 0
-        },
-        "desc": NaN,
-        "link": NaN
+        "link": "https://en.wikipedia.org/wiki/Liberalism"
     },
     {
         "name": "右翼民粹主义",
         "stats": {
             "econ": 40,
-            "govt": 30,
-            "scty": 30
+            "govt": 25,
+            "scty": 20
         },
         "desc": NaN,
-        "link": NaN
+        "link": "https://en.wikipedia.org/wiki/Right-wing_populism"
     },
     {
         "name": "法西斯主义",
         "stats": {
             "econ": 35,
-            "govt": 10,
+            "govt": 5,
             "scty": 15
         },
         "desc": NaN,
-        "link": NaN
+        "link": "https://en.wikipedia.org/wiki/Fascism"
     },
     {
         "name": "个人无政府主义",
@@ -187,7 +187,7 @@ ideologies = [
             "scty": 60
         },
         "desc": NaN,
-        "link": NaN
+        "link": "https://en.wikipedia.org/wiki/Individualist_anarchism"
     },
     {
         "name": "自由意志主义",
@@ -197,17 +197,17 @@ ideologies = [
             "scty": 60
         },
         "desc": NaN,
-        "link": NaN
+        "link": "https://en.wikipedia.org/wiki/Libertarianism"
     },
     {
         "name": "新自由主义",
         "stats": {
             "econ": 30,
-            "govt": 50,
-            "scty": 45
+            "govt": 55,
+            "scty": 50
         },
         "desc": NaN,
-        "link": NaN
+        "link": "https://en.wikipedia.org/wiki/Neoliberalism"
     },
     {
         "name": "保守主义",
@@ -217,47 +217,37 @@ ideologies = [
             "scty": 30
         },
         "desc": NaN,
-        "link": NaN
-    },
-    {
-        "name": "古典自由主义",
-        "stats": {
-            "econ": 20,
-            "govt": 60,
-            "scty": 60
-        },
-        "desc": NaN,
-        "link": NaN
+        "link": "https://en.wikipedia.org/wiki/Conservatism"
     },
     {
         "name": "威权资本主义",
         "stats": {
             "econ": 20,
             "govt": 15,
-            "scty": 35
+            "scty": 30
         },
         "desc": NaN,
-        "link": NaN
-    },
-    {
-        "name": "旧保守主义",
-        "stats": {
-            "econ": 20,
-            "govt": 60,
-            "scty": 20
-        },
-        "desc": NaN,
-        "link": NaN
+        "link": "https://en.wikipedia.org/wiki/Authoritarian_capitalism"
     },
     {
         "name": "新保守主义",
         "stats": {
             "econ": 20,
-            "govt": 35,
-            "scty": 20
+            "govt": 30,
+            "scty": 25
         },
         "desc": NaN,
-        "link": NaN
+        "link": "https://en.wikipedia.org/wiki/Neoconservatism"
+    },
+    {
+        "name": "古典自由主义",
+        "stats": {
+            "econ": 15,
+            "govt": 60,
+            "scty": 50
+        },
+        "desc": NaN,
+        "link": "https://en.wikipedia.org/wiki/Classical_liberalism"
     },
     {
         "name": "无政府资本主义",
@@ -267,7 +257,7 @@ ideologies = [
             "scty": 50
         },
         "desc": NaN,
-        "link": NaN
+        "link": "https://en.wikipedia.org/wiki/Anarcho-capitalism"
     },
     {
         "name": "最小政府主义",
@@ -277,6 +267,6 @@ ideologies = [
             "scty": 50
         },
         "desc": NaN,
-        "link": NaN
+        "link": "https://en.wikipedia.org/wiki/Night-watchman_state"
     }
 ];
