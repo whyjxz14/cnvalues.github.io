1e2adcac1ca76c4afed0bc2a30560bfa434056ef
CNValues <6501773-CNValues@users.noreply.gitlab.com>
2020-08-13 23:42:18 +0800

    0.5.8 b
    Adjust some ideologies.

--- info/ideologies.js
+++ info/ideologies.js
@@ -6,7 +6,7 @@ ideologies = [
             "govt": 100,
             "scty": 90
         },
-        "desc": "无政府共产主义是无政府主义中最主要的流派，主张废除国家机器和私有制，在自愿和互助的基础上建立一个各尽所能，按需分配的公有制社会。",
+        "desc": "无政府共产主义（Anarcho-Communism）是无政府主义中最主要的流派，主张废除国家机器和私有制，在自愿和互助的基础上建立一个各尽所能，按需分配的公有制社会。",
         "link": "https://en.wikipedia.org/wiki/Anarcho-communism"
     },
     {
@@ -16,7 +16,7 @@ ideologies = [
             "govt": 70,
             "scty": 90
         },
-        "desc": "正统马克思主义坚持激进的社会主义革命的必要性，同时强调社会主义民主和政治自由，反对改良主义和官僚化的集中制。",
+        "desc": "正统马克思主义（Orthodox Marxism）坚持激进的社会主义革命的必要性，同时强调社会主义民主和政治自由，反对改良主义和官僚化的集中制。",
         "link": "https://en.wikipedia.org/wiki/Orthodox_Marxism"
     },
     {
@@ -26,7 +26,7 @@ ideologies = [
             "govt": 50,
             "scty": 90
         },
-        "desc": "托洛茨基主义坚持布尔什维克革命初期宣告的工人民主和国际主义原则，强烈反对官僚化和一国社会主义论。",
+        "desc": "托洛茨基主义（Trotskyism）坚持布尔什维克革命初期宣告的工人民主和国际主义原则，强烈反对官僚化和一国社会主义论。",
         "link": "https://en.wikipedia.org/wiki/Trotskyism"
     },
     {
@@ -36,7 +36,7 @@ ideologies = [
             "govt": 30,
             "scty": 75
         },
-        "desc": "列宁主义主张建立民主集中制的先锋党并通过革命推翻资本主义，为了实现目标，不排斥临时的强制政策。",
+        "desc": "列宁主义（Leninism）主张建立民主集中制的先锋党并通过革命推翻资本主义，为了实现目标，不排斥临时的强制政策。",
         "link": "https://en.wikipedia.org/wiki/Leninism"
     },
     {
@@ -46,7 +46,7 @@ ideologies = [
             "govt": 5,
             "scty": 30
         },
-        "desc": "马克思列宁主义是斯大林时期苏联发展出的理论体系，其特点是一党专政，严厉的思想文化管制，一国社会主义论以及高度集中的计划经济。",
+        "desc": "马克思列宁主义（Marxism-Leninism）是斯大林时期苏联发展出的理论体系，其特点是一党专政，严厉的思想文化管制，一国社会主义论以及高度集中的计划经济。",
         "link": "https://en.wikipedia.org/wiki/Marxism%E2%80%93Leninism"
     },
     {
@@ -56,7 +56,7 @@ ideologies = [
             "govt": 90,
             "scty": 85
         },
-        "desc": "自由意志社会主义是社会主义运动中强调反对由中央集权国家控制社会和经济，主张基层工人自治的流派。",
+        "desc": "自由意志社会主义（Libertarian Socialism）是社会主义运动中强调反对由中央集权国家控制社会和经济，主张基层工人自治的流派。",
         "link": "https://en.wikipedia.org/wiki/Libertarian_socialism"
     },
     {
@@ -66,7 +66,7 @@ ideologies = [
             "govt": 70,
             "scty": 80
         },
-        "desc": "民主社会主义强调民主制在社会主义社会中的必要性，既反对先锋党模式，也不认同过激的无政府主义倾向。",
+        "desc": "民主社会主义（Democratic Socialism）强调民主制在社会主义社会中的必要性，既反对先锋党模式，也不认同过激的无政府主义倾向。",
         "link": "https://en.wikipedia.org/wiki/Democratic_socialism"
     },
     {
@@ -76,7 +76,7 @@ ideologies = [
             "govt": 90,
             "scty": 80
         },
-        "desc": "左翼自由意志主义在坚持自由意志主义的同时抵制资本主义的私有制和雇佣劳动，尤其反对私人占有自然资源。",
+        "desc": "左翼自由意志主义（Left-Libertarianism）在坚持自由意志主义的同时抵制资本主义的私有制和雇佣劳动，尤其反对私人占有自然资源。",
         "link": "https://en.wikipedia.org/wiki/Left-libertarianism"
     },
     {
@@ -86,18 +86,18 @@ ideologies = [
             "govt": 60,
             "scty": 70
         },
-        "desc": "社会民主主义主张对资本主义制度进行渐进式的改良，并强调这一改良运动本身的意义，而非特定的目标。",
+        "desc": "社会民主主义（Social Democracy）主张对资本主义制度进行渐进式的改良，并强调这一改良运动本身的意义，而非特定的目标。",
         "link": "https://en.wikipedia.org/wiki/Social_democracy"
     },
     {
-        "name": "左翼民粹主义",
+        "name": "左翼民族主义",
         "stats": {
             "econ": 70,
             "govt": 25,
             "scty": 50
         },
-        "desc": "左翼民粹主义主张加强国家对社会和经济的管控以保护底层民众，并且反对政治精英阶层的议会政治。",
-        "link": "https://en.wikipedia.org/wiki/Left-wing_populism"
+        "desc": "左翼民族主义（Left-Wing Nationalism）相信社会平等、人民主权、民族自决与解放，并且具有强烈的反帝国主义色彩，但也包含一定的族群偏见。",
+        "link": "https://en.wikipedia.org/wiki/Left-wing_nationalism"
     },
     {
         "name": "民族布尔什维克主义",
@@ -106,7 +106,7 @@ ideologies = [
             "govt": 0,
             "scty": 20
         },
-        "desc": "民族布尔什维克主义主张将苏联式的政治经济体制与法西斯的民族主义理念结合起来，反对民主，私有经济和国际主义。",
+        "desc": "民族布尔什维克主义（National Bolshevism）主张将苏联式的政治经济体制与法西斯的民族主义理念结合起来，反对民主，私有经济和国际主义。",
         "link": "https://en.wikipedia.org/wiki/National_Bolshevism"
     },
     {
@@ -116,7 +116,7 @@ ideologies = [
             "govt": 100,
             "scty": 60
         },
-        "desc": "无政府互助主义主张在自由市场的框架下将生产资料的使用权社会化，同时反对资本主义雇佣劳动。",
+        "desc": "无政府互助主义（Mutualism）主张在自由市场的框架下将生产资料的使用权社会化，同时反对资本主义雇佣劳动。",
         "link": "https://en.wikipedia.org/wiki/Mutualism_(economic_theory)"
     },
     {
@@ -126,7 +126,7 @@ ideologies = [
             "govt": 60,
             "scty": 70
         },
-        "desc": "社会自由主义是一种更加倾向于社会公平及经济干预的自由主义，并且尤其重视社会和文化的进步。",
+        "desc": "社会自由主义（Social Liberalism/New Liberalism）是一种更加倾向于社会公平及经济干预的自由主义，并且尤其重视社会和文化的进步。",
         "link": "https://en.wikipedia.org/wiki/Social_liberalism"
     },
     {
@@ -136,7 +136,7 @@ ideologies = [
             "govt": 45,
             "scty": 40
         },
-        "desc": "基督教民主主义强调传统价值观中反对个人利己主义而推崇集体主义的一面，并将其与现代自由民主制结合起来。",
+        "desc": "基督教民主主义（Christian Democracy）强调传统价值观中反对个人利己主义而推崇集体主义的一面，并将其与现代自由民主制结合起来。",
         "link": "https://en.wikipedia.org/wiki/Christian_democracy"
     },
     {
@@ -146,7 +146,7 @@ ideologies = [
             "govt": 0,
             "scty": 10
         },
-        "desc": "纳粹主义是法西斯主义的形式之一，推崇种族主义，并且比一般的法西斯主义更加倾向于国家统辖经济。",
+        "desc": "纳粹主义（Nazism）是法西斯主义的形式之一，推崇种族主义，并且比一般的法西斯主义更加倾向于国家统辖经济。",
         "link": "https://en.wikipedia.org/wiki/Nazism"
     },
     {
@@ -156,7 +156,7 @@ ideologies = [
             "govt": 55,
             "scty": 60
         },
-        "desc": "自由主义是以公民自由为基础的意识形态，主张政府的统治需要经法定秩序得到公民同意，以及保护自由贸易和个人权利。",
+        "desc": "自由主义（Liberalism）是以公民自由为基础的意识形态，主张政府的统治需要经法定秩序得到公民同意，以及保护自由贸易和个人权利。",
         "link": "https://en.wikipedia.org/wiki/Liberalism"
     },
     {
@@ -166,18 +166,18 @@ ideologies = [
             "govt": 100,
             "scty": 60
         },
-        "desc": "个人无政府主义者是自然法的忠实信徒，他们认为整个社会本身源自契约，并且个人有权利脱离一切契约，即脱离社会。",
+        "desc": "个人无政府主义（Individualist Anarchism）者是自然法的忠实信徒，他们认为整个社会本身源自契约，并且个人有权利脱离一切契约，即脱离社会。",
         "link": "https://en.wikipedia.org/wiki/Individualist_anarchism"
     },
     {
-        "name": "右翼民粹主义",
+        "name": "经济民族主义",
         "stats": {
             "econ": 40,
             "govt": 25,
             "scty": 20
         },
-        "desc": "右翼民粹主义反对建制派精英，同时反国际化，支持严格管控移民，它位于这个时代政治讨论的中心。",
-        "link": "https://en.wikipedia.org/wiki/Right-wing_populism"
+        "desc": "经济民族主义（Economic Nationalism）强调政府干预经济，认为市场应该服务于国家利益，并质疑当前的全球化与国际自由贸易，主张贸易保护主义。",
+        "link": "https://en.wikipedia.org/wiki/Economic_nationalism"
     },
     {
         "name": "法西斯主义",
@@ -186,7 +186,7 @@ ideologies = [
             "govt": 5,
             "scty": 15
         },
-        "desc": "法西斯主义是一种极端的民族主义，强调为了在国际竞争中保障本民族利益，国家必须实行铁腕的独裁统治，以掌控社会的各个方面。",
+        "desc": "法西斯主义（Fascism）是一种极端的民族主义，强调为了在国际竞争中保障本民族利益，国家必须实行铁腕的独裁统治，以掌控社会的各个方面。",
         "link": "https://en.wikipedia.org/wiki/Fascism"
     },
     {
@@ -196,7 +196,7 @@ ideologies = [
             "govt": 90,
             "scty": 60
         },
-        "desc": "自由意志主义主张外交上的孤立主义与经济上的充分放任，强调伦理正当性而非功利，以及对军警等国家暴力的抗拒。",
+        "desc": "自由意志主义（Libertarianism）主张外交上的孤立主义与经济上的充分放任，强调伦理正当性而非功利，以及对军警等国家暴力的抗拒。",
         "link": "https://en.wikipedia.org/wiki/Libertarianism"
     },
     {
@@ -206,7 +206,7 @@ ideologies = [
             "govt": 55,
             "scty": 50
         },
-        "desc": "新自由主义主张大规模的私有化和缩小政府，同时在外交方面依靠政府组建贸易同盟和对敌经济制裁来保证自由主义的影响力。",
+        "desc": "新自由主义（Neoliberalism）主张大规模的私有化和缩小政府，同时在外交方面依靠政府组建贸易同盟和对敌经济制裁来保证自由主义的影响力。",
         "link": "https://en.wikipedia.org/wiki/Neoliberalism"
     },
     {
@@ -216,7 +216,7 @@ ideologies = [
             "govt": 40,
             "scty": 30
         },
-        "desc": "保守主义源于法国大革命的反对派，它推崇传统的宗教价值、政治体制与财产分配，意在维护社会稳定和延续。",
+        "desc": "保守主义（Conservatism）源于法国大革命的反对派，它推崇传统的宗教价值、政治体制与财产分配，意在维护社会稳定和延续。",
         "link": "https://en.wikipedia.org/wiki/Conservatism"
     },
     {
@@ -226,7 +226,7 @@ ideologies = [
             "govt": 30,
             "scty": 25
         },
-        "desc": "新保守主义认为民主自由来源于国家传统，因而主张激进地压制文化左翼，捍卫传统，构建爱国主义，制裁国际反民主势力。",
+        "desc": "新保守主义（Neoconservatism）认为民主自由来源于国家传统，因而主张激进地压制文化左翼，捍卫传统，构建爱国主义，制裁国际反民主势力。",
         "link": "https://en.wikipedia.org/wiki/Neoconservatism"
     },
     {
@@ -236,7 +236,7 @@ ideologies = [
             "govt": 15,
             "scty": 30
         },
-        "desc": "威权资本主义是威权体制下的资本主义市场经济，限制个人的自由和政治权利，其政府通常长期由一党领导。",
+        "desc": "威权资本主义（Authoritarian Capitalism）是威权体制下的资本主义市场经济，限制个人的自由和政治权利，其政府通常长期由一党领导。",
         "link": "https://en.wikipedia.org/wiki/Authoritarian_capitalism"
     },
     {
@@ -246,7 +246,7 @@ ideologies = [
             "govt": 65,
             "scty": 50
         },
-        "desc": "古典自由主义强调法治下的公民自由，特别是经济自由，它主张自由放任的经济政策，并认为政府的职能在于保护这些自由。",
+        "desc": "古典自由主义（Classical Liberalism）强调法治下的公民自由，特别是经济自由，它主张自由放任的经济政策，并认为政府的职能在于保护这些自由。",
         "link": "https://en.wikipedia.org/wiki/Classical_liberalism"
     },
     {
@@ -256,17 +256,17 @@ ideologies = [
             "govt": 100,
             "scty": 50
         },
-        "desc": "无政府资本主义主张取消国家机器，按照互不侵犯原则组织社会，以保障私有制和市场经济的自由发展。",
+        "desc": "无政府资本主义（Anarcho-Capitalism）主张取消国家机器，按照互不侵犯原则组织社会，以保障私有制和市场经济的自由发展。",
         "link": "https://en.wikipedia.org/wiki/Anarcho-capitalism"
     },
     {
-        "name": "最小政府主义",
+        "name": "小政府主义",
         "stats": {
             "econ": 0,
             "govt": 85,
             "scty": 50
         },
-        "desc": "最小政府主义主张政府的唯一功能是通过军警和法院来捍卫互不侵犯原则，从而保护公民免遭侵略、盗窃、违约、欺诈并强制执行财产法。",
+        "desc": "小政府主义（Minarchism）主张政府的唯一功能是通过军警和法院来捍卫互不侵犯原则，从而保护公民免遭侵略、盗窃、违约、欺诈并强制执行财产法。",
         "link": "https://en.wikipedia.org/wiki/Night-watchman_state"
     }
 ];
